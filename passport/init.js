3c0f524d07b040bd938e07760171066aed301e4b
Manvendra Singh <mjdeora@gmail.com>
2018-01-06 10:52:29 +0530

    Updated package.json and fix some typo

--- passport/init.js
+++ passport/init.js
@@ -3,11 +3,12 @@ const signup = require('./signup.js');
 const User = require('../model/user');
 
 module.exports = (passport) => {
+    // Serialize User for Session
     passport.serializeUser(function(user, done) {
         console.log('serializing user: '+user);
         done(null, user._id);
     });
-
+    // Deserialize User from Session
     passport.deserializeUser(function(id, done) {
         User.findById(id, function(err, user) {
             console.log('deserializing user:',user);
@@ -15,6 +16,7 @@ module.exports = (passport) => {
         });
     });
 
+    // Calling Login and Signup and Passing passport object
     login(passport);
     signup(passport);
 }
